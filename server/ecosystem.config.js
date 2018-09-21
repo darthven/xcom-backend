59f4320c981bd0de5027a17e02b1b5f0e83e9bd4
Alexander Stepanov <vaiol.ans@gmail.com>
2018-09-21 16:42:10 +0300

    image folder config updated

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -6,12 +6,13 @@ module.exports = {
       watch: false,
       env: {
         NODE_ENV: 'production',
-        PORT: 1444,
+        PORT: 1340,
         ECOM_URL: 'http://ws.erkapharm.com:8990/ecom/hs/',
         ECOM_USER: 'IdEast',
         ECOM_PASS: 'KZMY5N',
         MONGO_URI: 'mongodb://localhost:27017',
-        MONGO_DB: 'xcom-prod'
+        MONGO_DB: 'xcom-prod',
+        IMAGE_FOLDER: '/var/www/xcom/static/images/'
       }
     }
   ]
