45517b4a926efaac104d0dc3c3b0e3b10f0ac136
bolein <bolein95@gmail.com>
2018-11-13 12:37:29 -0600

    stocks index upd

--- server/src/mongo/repository/stocks.ts
+++ server/src/mongo/repository/stocks.ts
@@ -10,5 +10,7 @@ export class StocksRepository extends Repository {
     public async createCollection(): Promise<void> {
         await super.createCollection()
         await this.collection.createIndex({ goodsId: 1 })
+        // unique compound index
+        await this.collection.createIndex({ storeId: 1, goodsId: 1, batch: 1 }, { unique: true })
     }
 }
