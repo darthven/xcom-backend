ba15939485d779ad30d978b4372159e89db49947
darthven <kostharl@gmail.com>
2018-11-15 14:33:54 +0200

    Docs were added

--- server/src/manzana/manzanaChequeItem.ts
+++ server/src/manzana/manzanaChequeItem.ts
@@ -2,16 +2,55 @@ import { IsDefined, IsInt } from 'class-validator'
 import { Item } from '../common/item'
 
 export interface ManzanaChequeItem extends Item {
-    // TODO: additional fields returned with manzana cheque
-    // TODO: per-item bonus?
-    price: number // Цена
-    amount: number // Сумма по строке. (Если передается сумма по строке и она меньше кол-во*цена, то рассчитывается скидка)
-    discount: number // Сумма скидки. (Если не передается сумма по строке, то сумма рассчитывается кол-во*цена - сумма скидки)
+    /**
+     * Цена
+     */
+    price: number
+
+    /**
+     * Сумма по строке. (Если передается сумма по строке
+     * и она меньше кол-во * цена, то рассчитывается скидка)
+     */
+    amount: number
+
+    /**
+     * Сумма скидки. (Если не передается сумма по строке,
+     * то сумма рассчитывается кол-во*цена - сумма скидки)
+     */
+    discount: number
+
+    /**
+     * Доступный платеж в деньгах
+     */
     availablePayment: number
+
+    /**
+     * Всего начислено баллов
+     */
     chargedBonus: number
+
+    /**
+     * Начислено статусных баллов
+     */
     chargedStatusBonus: number
+
+    /**
+     * Всего списано баллов
+     */
     writeoffBonus: number
+
+    /**
+     * Списано статусных баллов
+     */
     writeoffStatusBonus: number
+
+    /**
+     * Всего начислено активных бонусных баллов
+     */
     activeChargedBonus: number
+
+    /**
+     * Всего начислено активных статусных бонусных баллов
+     */
     activeChargedStatusBonus: number
 }
