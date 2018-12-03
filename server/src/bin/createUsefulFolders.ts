d7a9b8a3fdcf3ab5f269e8a1aa4ad22801454195
bolein <bolein95@gmail.com>
2018-12-03 10:06:35 -0600

    upd fs script

--- server/src/bin/createUsefulFolders.ts
+++ server/src/bin/createUsefulFolders.ts
@@ -15,16 +15,14 @@ import logger from '../config/logger.config'
 
 const dirs = [`${path}/${IMAGE_TMP_FOLDER}`, `${path}/${LOGS_FOLDER}`]
 
-if (NODE_ENV !== 'production') {
-    dirs.push(
-        `${path}/${IMAGE_FOLDER}`,
-        `${path}/${IMAGE_FOLDER}${IMAGE_CATEGORIES_FOLDER}`,
-        `${path}/${IMAGE_FOLDER}${IMAGE_STORE_TYPE_FOLDER}`,
-        `${path}/${IMAGE_FOLDER}${IMAGE_GOOD_FOLDER}`,
-        `${path}/${IMAGE_FOLDER}${IMAGE_GOOD_FOLDER}${IMAGE_M_SUBFOLDER}`,
-        `${path}/${IMAGE_FOLDER}${IMAGE_GOOD_FOLDER}${IMAGE_S_SUBFOLDER}`
-    )
-}
+dirs.push(
+    `${IMAGE_FOLDER}`,
+    `${IMAGE_FOLDER}${IMAGE_CATEGORIES_FOLDER}`,
+    `${IMAGE_FOLDER}${IMAGE_STORE_TYPE_FOLDER}`,
+    `${IMAGE_FOLDER}${IMAGE_GOOD_FOLDER}`,
+    `${IMAGE_FOLDER}${IMAGE_GOOD_FOLDER}${IMAGE_M_SUBFOLDER}`,
+    `${IMAGE_FOLDER}${IMAGE_GOOD_FOLDER}${IMAGE_S_SUBFOLDER}`
+)
 
 dirs.forEach(dir => {
     if (!fs.existsSync(dir)) {
