44ae83fb6a069969ba326aa8c20e6e929e14f558
bolein <bolein95@gmail.com>
2018-12-09 22:55:03 -0600

    ERKA-661 added filter for goods that are in stock

--- server/src/mongo/queries/GoodsFilter.ts
+++ server/src/mongo/queries/GoodsFilter.ts
@@ -2,6 +2,7 @@ import { ProductFilter } from '../../parameters/productFilter'
 
 export class GoodsFilter {
     public siteCatId?: { $in: number[] }
+    public 'price.stores'?: { $in: number[] }
     public price?: any
     public 'share.id'?: { $in: number[] }
     public 'share.endDate'?: { $gt: Date }
@@ -15,16 +16,17 @@ export class GoodsFilter {
             this['share.id'] = { $in: filter.shares }
             this['share.endDate'] = { $gt: new Date() }
         }
-        if (filter.priceMin || filter.priceMax) {
-            this.price = {
-                $elemMatch: {
-                    region,
-                    priceMin: { $lt: filter.priceMax || 1000000 },
-                    priceMax: { $gt: filter.priceMin || 0 }
-                }
+        this.price = {
+            $elemMatch: {
+                region
             }
-        } else {
-            this.price = { $elemMatch: { region } }
+        }
+        if (filter.inStock && filter.storeIds) {
+            this.price.$elemMatch.stores = { $in: filter.storeIds }
+        }
+        if (filter.priceMin || filter.priceMax) {
+            this.price.$elemMatch.priceMin = { $lt: filter.priceMax || 1000000 }
+            this.price.$elemMatch.priceMax = { $gt: filter.priceMin || 0 }
         }
     }
 }
