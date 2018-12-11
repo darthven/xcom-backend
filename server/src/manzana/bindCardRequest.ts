d2938f24cd7c6f3c3dbeed6ea487383860123b5c
bolein <bolein95@gmail.com>
2018-12-11 23:21:55 +0200

    ERKA-696 partner id

--- server/src/manzana/bindCardRequest.ts
+++ server/src/manzana/bindCardRequest.ts
@@ -2,6 +2,7 @@ export class BindCardRequest {
     public Id: string
     public CardNumber: string
     public SessionId: string
+    public readonly PartnerId = '5C0F2C5E-69A6-E611-80D2-00155DFA8043'
 
     constructor(CardNumber: string, Id: string, SessionId: string) {
         this.Id = Id
