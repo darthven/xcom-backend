b8381030a807a09d13c38130753416bc34920c73
darthven <kostharl@gmail.com>
2018-12-13 19:04:20 +0200

    Shares were fixed

--- server/src/mongo/entity/share.ts
+++ server/src/mongo/entity/share.ts
@@ -14,14 +14,14 @@ export class Share {
     public regions: number[]
 
     constructor(csvRowData: CSVData) {
-        this.id = parseInt(csvRowData.ID, 10)
-        this.goodId = parseInt(csvRowData['Код А2005'], 10)
-        this.discountValue = parseInt(csvRowData.Значение, 10)
+        this.id = parseInt(csvRowData['ID акции'], 10)
+        this.goodId = parseInt(csvRowData.КодА2005, 10)
+        this.discountValue = parseInt(csvRowData['Значение скидки'], 10)
         this.packCount = parseInt(csvRowData['Кол-во упаковок'], 10)
         this.attributeZOZ = !!parseInt(csvRowData['Признак ЗОЗ'], 10)
         this.startDate = moment(csvRowData['Дата начала'], 'DD.MM.YYYY').toDate()
         this.endDate = moment(csvRowData['Дата окончания'], 'DD.MM.YYYY').toDate()
-        this.description = csvRowData['Описание акции на сайт']
-        this.regions = csvRowData['Регионы для показа на сайте'].split(' ').map(value => parseInt(value, 10))
+        this.description = csvRowData['Описание акции']
+        this.regions = csvRowData.Регион.split(' ').map(value => parseInt(value, 10))
     }
 }
