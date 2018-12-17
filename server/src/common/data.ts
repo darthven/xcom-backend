49aab4efd788ecc162f817e918d90fbd0d24dfb6
darthven <kostharl@gmail.com>
2018-12-17 18:04:55 +0200

    Categories sorting was fixed

--- server/src/common/data.ts
+++ server/src/common/data.ts
@@ -7,4 +7,47 @@ export const ADMINS: Admin[] = [
     new Admin('test', bcrypt.hashSync('test', bcrypt.genSaltSync(10)))
 ]
 
+export const orderedCategoriesRoots: Array<{ id: number; order: number }> = [
+    {
+        id: 7,
+        order: 0
+    },
+    {
+        id: 6,
+        order: 1
+    },
+    {
+        id: 9,
+        order: 2
+    },
+    {
+        id: 3,
+        order: 3
+    },
+    {
+        id: 8,
+        order: 4
+    },
+    {
+        id: 2,
+        order: 5
+    },
+    {
+        id: 5,
+        order: 6
+    },
+    {
+        id: 14,
+        order: 7
+    },
+    {
+        id: 10,
+        order: 8
+    },
+    {
+        id: 13,
+        order: 9
+    }
+]
+
 export const TECHNICAL_CARD = '9419410000035'
