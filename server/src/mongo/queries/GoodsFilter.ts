c0c84c446e0701ad79cdb08c68997c63cd2ee0d3
bolein <bolein95@gmail.com>
2018-12-20 00:00:49 +0200

    filter out not in stock again :(

--- server/src/mongo/queries/GoodsFilter.ts
+++ server/src/mongo/queries/GoodsFilter.ts
@@ -18,16 +18,17 @@ export class GoodsFilter {
             this['share.endDate'] = { $gt: new Date() }
             this.$or = [{ 'share.regions': { $exists: true, $eq: null } }, { 'share.regions': region }]
         }
-        if (filter.inStock && filter.storeIds) {
-            this.price = {
-                $elemMatch: {
-                    stores: { $in: filter.storeIds }
-                }
+        this.price = {
+            $elemMatch: {
+                region
             }
         }
+        if (filter.inStock && filter.storeIds) {
+            this.price.$elemMatch.stores = { $in: filter.storeIds }
+        }
         if (filter.priceMin || filter.priceMax) {
-            this.price = this.price || { $elemMatch: {} }
-            this.price.$elemMatch.region = region
+            // this.price = this.price || { $elemMatch: {} }
+            // this.price.$elemMatch.region = region
             this.price.$elemMatch.priceMin = { $lt: filter.priceMax || 1000000 }
             this.price.$elemMatch.priceMax = { $gt: filter.priceMin || 0 }
         }
